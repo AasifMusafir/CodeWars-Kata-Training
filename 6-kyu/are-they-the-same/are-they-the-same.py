0272b0e1657c481e833c60f946dbe99a66066676
Asifkhan musafir <151855456+AasifMusafir@users.noreply.github.com>
2026-03-25 14:13:53 +0530

    Add solution - CodeHub

---
+++ 6-kyu/are-they-the-same/are-they-the-same.py
@@ -0,0 +1,8 @@
+def comp(array1, array2):
+    if array1 is None or array2 is None:
+        return False
+    
+    if len(array1) != len(array2):
+        return False
+    
+    return sorted([i*i for i in array1]) == sorted(array2)
